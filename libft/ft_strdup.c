2577dbda7fe2d0b9121f5f9fd19d9c07c28c6cbf
Nemo Gaulthier <ngaulthi@z4r9p7.42lyon.fr>
2024-04-30 12:31:13 +0200

    0

---
+++ libft/ft_strdup.c
@@ -0,0 +1,41 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strdup.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ngaulthi <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/11/17 14:00:00 by ngaulthi          #+#    #+#             */
+/*   Updated: 2023/11/17 14:10:39 by ngaulthi         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+static	int	strlenght(const char *src)
+{
+	int	i;
+
+	i = 0;
+	while (src[i])
+		i++;
+	return (i);
+}
+
+char	*ft_strdup(const char *s)
+{
+	char	*new;
+	int		i;
+
+	i = 0;
+	new = malloc(sizeof(char) * strlenght (s) + 1);
+	if (!new)
+		return (NULL);
+	while (s[i])
+	{
+		new[i] = s[i];
+		i++;
+	}
+	new[i] = '\0';
+	return (new);
+}
